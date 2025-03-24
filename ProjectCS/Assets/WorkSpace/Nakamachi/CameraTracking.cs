7cd26e3a2e99c1aa2554bbd74b5287d034dc74f5
Raiga1119 <marvel_0819@icloud.com>
2025-03-24 22:36:28 +0900

    CameraTracking.csのコメント修正

--- ProjectCS/Assets/WorkSpace/Nakamachi/CameraTracking.cs
+++ ProjectCS/Assets/WorkSpace/Nakamachi/CameraTracking.cs
@@ -11,22 +11,35 @@ using UnityEngine;
 
 public class CameraTracking : MonoBehaviour
 {
+    //プレイヤーオブジェクトを格納する変数
     private GameObject player;
-    public float yOffset; //y軸方向のオフセット
-    public float zOffset; //z軸方向のオフセット
+
+    //カメラのy軸方向のオフセット
+    public float yOffset;
+
+    //カメラのz軸方向のオフセット
+    public float zOffset;
 
     // Start is called before the first frame update
     void Start()
     {
+        //Playerという名前のオブジェクトをシーンから探してplayerに格納
         player = GameObject.Find("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
+        //プレイヤーの現在のx座標を取得
         float x = player.transform.position.x;
+
+        //プレイヤーの現在のy座標を取得
         float y = player.transform.position.y;
+
+        //プレイヤーの現在のz座標を取得
         float z = player.transform.position.z;
+
+        //カメラの位置をプレイヤーの位置にオフセットを加えた位置に設定
         transform.position = new Vector3(x, y + yOffset, z + zOffset);
     }
 }
