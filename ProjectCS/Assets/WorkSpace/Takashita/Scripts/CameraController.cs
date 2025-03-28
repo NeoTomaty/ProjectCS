ffee0b7bf4b69b7318f06ec05bc5bb346a703545
dakkie <yon4daiki@gmail.com>
2025-03-28 22:39:57 +0900

    03/27 Takashita/にAssets,Prefabs,Scriptsファイル作成 Prefabs/にPlayer,Ground,PlayerCamera追加 Scripts/にAutoMovePlayer,CameraController,LRMovePlayer追加

---
+++ ProjectCS/Assets/WorkSpace/Takashita/Scripts/CameraController.cs
@@ -0,0 +1,25 @@
+//====================================================
+// スクリプト名：CameraController
+// 作成者：高下
+// 内容：プレイヤーへのカメラ追従
+// 最終更新日：03/27
+// 
+// [Log]
+// 03/27 高下 スクリプト作成 
+//====================================================
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
