ee68c8dfb937c2ff8dba399d01603522fc865c59
NeoTomaty <tokito1025@gmail.com>
2025-03-29 13:40:30 +0900

    竹内　一度mainブランチを元に戻す

--- ProjectCS/Assets/WorkSpace/Nakamachi/Script/CameraTracking.cs
+++ ProjectCS/Assets/WorkSpace/Nakamachi/Script/CameraTracking.cs
@@ -1,9 +1,8 @@
-//ファイル名:CameraTracking.cs
+//スクリプト名:CameraTracking.cs
 //作成者:中町雷我
-//作成日:2025/03/24
-//更新履歴:
-//2025/03/24:初版作成
-//概要:プレイヤーにカメラ追従機能を実装
+//最終更新日:2025/03/24
+//[Log]
+//2025/03/24　中町雷我　プレイヤーにカメラ追従機能を実装
 
 using System.Collections;
 using System.Collections.Generic;
@@ -11,22 +10,35 @@ using UnityEngine;
 
 public class CameraTracking : MonoBehaviour
 {
+    //プレイヤーオブジェクトを格納する変数
     private GameObject player;
-    public float yOffset; //y軸方向のオフセット
-    public float zOffset; //z軸方向のオフセット
+
+    //カメラのy軸方向のオフセット
+    public float yOffset;
+
+    //カメラのz軸方向のオフセット
+    public float zOffset;
 
     // Start is called before the first frame update
     void Start()
     {
+        //Playerという名前のオブジェクトをシーンから探してplayerに格納
         player = GameObject.Find("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
+        //プレイヤーの現在のx座標を取得
         float x = player.transform.position.x;
+
+        //プレイヤーの現在のy座標を取得
         float y = player.transform.position.y;
+
+        //プレイヤーの現在のz座標を取得
         float z = player.transform.position.z;
+
+        //カメラの位置をプレイヤーの位置にオフセットを加えた位置に設定
         transform.position = new Vector3(x, y + yOffset, z + zOffset);
     }
 }
