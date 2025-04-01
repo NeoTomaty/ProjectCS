588b352f58c0f19189b7d9f40992bad829dbbd3e
NORIGAKONOTE\gaiji397 <gaiji397@gmail.com>
2025-04-01 20:03:58 +0900

    森脇 Enemy1 回転 射撃 吹き飛ばし 消滅 実装

---
+++ ProjectCS/Assets/WorkSpace/Moriwaki/Script/EnemyBullet.cs
@@ -0,0 +1,39 @@
+﻿//======================================================
+// [�X�N���v�g��]Enemy1
+// �쐬�ҁF�X�e���S
+// �ŏI�X�V���F4/01
+//
+// [Log]
+// 3/31  �X�e�@�X�N���v�g�쐬
+//======================================================
+
+using UnityEngine;
+
+public class EnemyBullet : MonoBehaviour
+{
+    public int damage = 1;  // ダメージ
+    public string playerTag = "Player"; // プレイヤーのタグ名を指定
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // 衝突したオブジェクトのタグがプレイヤータグと一致する場合
+        if (collision.gameObject.CompareTag(playerTag))
+        {
+            // Player クラスが見つからない場合に備えて TryGetComponent を使用
+            if (collision.gameObject.TryGetComponent(out Component playerComponent))
+            {
+                var takeDamageMethod = playerComponent.GetType().GetMethod("TakeDamage");
+                if (takeDamageMethod != null)
+                {
+                    takeDamageMethod.Invoke(playerComponent, new object[] { damage });
+                }
+            }
+            Destroy(gameObject); // 弾を消す
+        }
+    }
+
+    private void Start()
+    {
+        Destroy(gameObject, 30f); // 30秒後に消える
+    }
+}
---
+++ ProjectCS/Assets/WorkSpace/Moriwaki/Script/EnemyPlayerTest.cs
@@ -0,0 +1,60 @@
+﻿//======================================================
+// [�X�N���v�g��]Enemy1
+// �쐬�ҁF�X�e���S
+// �ŏI�X�V���F4/01
+//
+// [Log]
+// 3/31  �X�e�@�X�N���v�g�쐬
+//======================================================
+
+using UnityEngine;
+
+public class EnemyPlayerTest : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public int maxHealth = 100;
+    private int currentHealth;
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        currentHealth = maxHealth;
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+        Vector3 move = new Vector3(moveX, 0, moveZ) * moveSpeed;
+        rb.linearVelocity = new Vector3(move.x, rb.linearVelocity.y, move.z);
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        Debug.Log("Player HP: " + currentHealth);
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        Debug.Log("Player Died!");
+        Respawn();
+    }
+
+    private void Respawn()
+    {
+        transform.position = Vector3.zero; // 仮のリスポーン地点
+        currentHealth = maxHealth;
+    }
+}
