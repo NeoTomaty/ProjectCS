0722a15c18f14d9d7c606093145b759313198c29
dakkie <yon4daiki@gmail.com>
2025-04-08 22:34:02 +0900

    高下 Arai/Script/PlayerHitWall.cs 一部修正 高速時による壁貫通防止処理を実装 速度の上限を制御する処理を追加

--- ProjectCS/Assets/WorkSpace/Arai/Script/PlayerHitWall.cs
+++ ProjectCS/Assets/WorkSpace/Arai/Script/PlayerHitWall.cs
@@ -1,29 +1,40 @@
 //======================================================
 // [PlayerHitWall]
 // 作成者：荒井修
-// 最終更新日：03/31
+// 最終更新日：04/08
 // 
 // [Log]
-// 3/31　荒井　プレイヤーが壁に衝突した際の挙動を作成
-// 3/31　荒井　移動の仮スクリプトを自作し動作を確認
-// 4/01　荒井　Playerオブジェクトの本スクリプトに対応
+// 03/31　荒井　プレイヤーが壁に衝突した際の挙動を作成
+// 03/31　荒井　移動の仮スクリプトを自作し動作を確認
+// 04/01　荒井　Playerオブジェクトの本スクリプトに対応
+// 04/08　髙下　壁反射の仕様を変更
 //======================================================
 
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class PlayerHitWall : MonoBehaviour
 {
     [Tooltip("壁に衝突した際の加速量")]
     [SerializeField] private float Acceleration = 1.0f;
 
+    [SerializeField]
+    private float MaxVelocityY = 50.0f;  // Y軸最大の速さ
+    [SerializeField]
+    private float MinVelocityY = -50.0f; // Y軸最小の速さ
+
+    private bool IsJumpReflect = false;  // ジャンプ時の壁反射で力を加えるかどうか
+
+    private Rigidbody Rb;    // プレイヤーのRigidbody
+
     // プレイヤーの移動方向と速度にアクセスするための変数
     // 同じオブジェクトにアタッチされているスクリプトであるという想定での実装
     MovePlayer MovePlayerScript;    //実際のスクリプト
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         MovePlayerScript = GetComponent<MovePlayer>();
+        Rb = GetComponent<Rigidbody>(); // Rigidbodyを取得
 
         if (MovePlayerScript == null)
         {
@@ -31,12 +42,7 @@ public class PlayerHitWall : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+   
     private void OnCollisionEnter(Collision collision)
     {
         if (MovePlayerScript == null) return;
@@ -53,13 +59,39 @@ public class PlayerHitWall : MonoBehaviour
             Vector3 Normal = collision.contacts[0].normal;
 
             // 反射ベクトルを計算
-            Vector3 Reflect = Vector3.Reflect(PlayerMoveDirection, Normal);
+            Vector3 Reflect = Vector3.Reflect(PlayerMoveDirection, Normal).normalized;
 
             // 反射ベクトルをプレイヤーに適用
             MovePlayerScript.SetMoveDirection(Reflect);
 
             // プレイヤーを加速
             MovePlayerScript.PlayerSpeedManager.SetAccelerationValue(Acceleration);
+
+            if (!IsJumpReflect) return;
+
+            // 壁反射後の力の方向をVelocityに応じて決定する
+            Reflect = new Vector3(0.0f, Mathf.Clamp(Rb.linearVelocity.y, MinVelocityY, MaxVelocityY), 0.0f);
+
+            // 壁反射後に一定の力を加える
+            Rb.AddForce(Reflect, ForceMode.Impulse);
+
+            // 壁反射時のAddForceを無効にする
+            IsJumpReflect = false;
+        }
+        // 地面に着いた場合は変化したMoveDirectionYを0にする
+        else if (collision.gameObject.tag == "Ground")
+        {
+            // プレイヤーの移動ベクトルを取得
+            Vector3 PlayerMoveDirection = MovePlayerScript.GetMoveDirection;
+
+            // PlayerMoveDirection.yを0にリセット
+            PlayerMoveDirection.y = 0.0f;
+
+            // PlayerMoveDirectionの更新
+            MovePlayerScript.SetMoveDirection(PlayerMoveDirection);
+
+            // 壁反射時のAddForceを有効にする
+            IsJumpReflect = true;
         }
     }
 }
