355e49559f8fc45c500cb46d39dfc1e5010937a7
Raiga1119 <marvel_0819@icloud.com>
2025-04-15 00:27:28 +0900

    中町　Enterキーでもシーン遷移できるようにする処理追加

--- ProjectCS/Assets/WorkSpace/Nakamachi/Script/SceneChanger.cs
+++ ProjectCS/Assets/WorkSpace/Nakamachi/Script/SceneChanger.cs
@@ -3,10 +3,12 @@
 //最終更新日:2025/04/11
 //[Log]
 //04/11　中町　TestResultSceneからTestGameSceneに遷移する処理
+//04/15　中町　Enterキーでもシーン遷移できるようにする処理追加
 
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.InputSystem;
+using Unity.VisualScripting;
 
 public class SceneChanger : MonoBehaviour
 {
@@ -15,6 +17,13 @@ public class SceneChanger : MonoBehaviour
     {
         //XboxコントローラーのAボタンが押されたかどうかをチェック
         if (Gamepad.current != null && Gamepad.current.aButton.wasPressedThisFrame)
+        {
+            //TestGameSceneに移動
+            SceneManager.LoadScene("TestGameScene");
+        }
+
+        //Enterキーが押されたかどうかをチェック
+        if (Keyboard.current!=null&&Keyboard.current.enterKey.wasPressedThisFrame)
         {
             //TestGameSceneに移動
             SceneManager.LoadScene("TestGameScene");
