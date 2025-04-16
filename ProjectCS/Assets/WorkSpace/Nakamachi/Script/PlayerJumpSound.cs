c6ca37b056b6ff01bf4390d353ba12192b5fac8c
Raiga1119 <marvel_0819@icloud.com>
2025-04-16 18:41:58 +0900

    Revert "中町　Playerが着地したときの着地音を一回だけ鳴らす処理に修正"
    This reverts commit 1261ea5a28bde6cbe567a53487ed9dda33c31caf.

--- ProjectCS/Assets/WorkSpace/Nakamachi/Script/PlayerJumpSound.cs
+++ ProjectCS/Assets/WorkSpace/Nakamachi/Script/PlayerJumpSound.cs
@@ -9,50 +9,34 @@ using UnityEngine;
 
 public class PlayerJumpSound : MonoBehaviour
 {
-    //ジャンプ音のAudioClipを設定するための変数
     public AudioClip JumpSound;
-
-    //AudioSourceコンポーネントを保持するための変数
     private AudioSource AudioSource;
-
-    //プレイヤーが地面にいるかどうかを判定するための変数
     private bool IsGrounded = true;
 
     void Start()
     {
-        //AudioSourceコンポーネントを取得
         AudioSource = GetComponent<AudioSource>();
     }
 
-    //プレイヤーが地面から離れたときに呼ばれるメソッド
     void OnCollisionExit(Collision collision)
     {
-        //衝突したオブジェクトがGroundタグを持っていて、プレイヤーが地面にいたとき
         if (collision.gameObject.CompareTag("Ground") && IsGrounded)
         {
-            //プレイヤーが地面から離れたとき
             IsGrounded = false;
-
-            //ジャンプ音を再生
             PlayJumpSound();
         }
     }
 
-    //プレイヤーが地面に着地したときに呼ばれるメソッド
     void OnCollisionEnter(Collision collision)
     {
-        //衝突したオブジェクトがGroundタグを持っているとき
         if (collision.gameObject.CompareTag("Ground"))
         {
-            //プレイヤーが地面にいるとき
             IsGrounded = true;
         }
     }
 
-    //ジャンプ音を再生するメソッド
     void PlayJumpSound()
     {
-        //AudioSourceコンポーネントを使用してジャンプ音を再生
         AudioSource.PlayOneShot(JumpSound);
     }
 }
--- ProjectCS/Assets/WorkSpace/Nakamachi/Script/PlayerLandingSound.cs
+++ ProjectCS/Assets/WorkSpace/Nakamachi/Script/PlayerLandingSound.cs
@@ -3,56 +3,28 @@
 //最終更新日:2025/04/15
 //[Log]
 //04/15　中町　Playerが着地したときの効果音処理
-//04/16　中町　着地したら一回だけ鳴らす処理に修正
 
 using UnityEngine;
 
 public class PlayerLandingSound : MonoBehaviour
 {
-    //着地音のAudioClipを設定するための変数
-    public AudioClip LandingSound;
+    public AudioSource LandingSound;
+    private bool IsGrounded;
+    private bool WasGrounded;
 
-    //AudioSourceコンポーネントを保持するための変数
-    private AudioSource AudioSource;
-
-    //プレイヤーが着地したかどうかを判定するための変数
-    private bool HasLanded = false;
-
-    void Start()
+    // Update is called once per frame
+    void Update()
     {
-        //AudioSourceコンポーネントを取得
-        AudioSource = GetComponent<AudioSource>();
-    }
+        //地面にいるかどうかをチェック
+        IsGrounded = Physics.Raycast(transform.position, Vector3.down, 1.1f);
 
-    //プレイヤーが地面に着地したときに呼ばれるメソッド
-    void OnCollisionEnter(Collision collision)
-    {
-        //衝突したオブジェクトがGroundタグを持っていて、まだ着地していないとき
-        if (collision.gameObject.CompareTag("Ground") && !HasLanded)
+        //着地したとき
+        if(!WasGrounded&&IsGrounded)
         {
-            //プレイヤーが着地したとき
-            HasLanded = true;
-
-            //着地音を再生
-            PlayLandingSound();
+            LandingSound.Play();
         }
-    }
 
-    //プレイヤーが地面から離れたときに呼ばれるメソッド
-    void OnCollisionExit(Collision collision)
-    {
-        //衝突したオブジェクトがGroundタグを持っているとき
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            //プレイヤーが地面から離れたとき
-            HasLanded = false;
-        }
-    }
-
-    //着地音を再生するメソッド
-    void PlayLandingSound()
-    {
-        //AudioSourceコンポーネントを使用して着地音を再生
-        AudioSource.PlayOneShot(LandingSound);
+        //前のフレームの状態を更新
+        WasGrounded = IsGrounded;
     }
 }
