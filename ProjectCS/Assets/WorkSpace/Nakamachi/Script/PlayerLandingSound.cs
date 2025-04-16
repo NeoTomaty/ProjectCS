ca3401f6369bfc065c4493192fd9dbc962d4c766
Raiga1119 <marvel_0819@icloud.com>
2025-04-16 18:44:28 +0900

    Revert "Revert "中町　Playerが着地したときの着地音を一回だけ鳴らす処理に修正""
    This reverts commit c6ca37b056b6ff01bf4390d353ba12192b5fac8c.

--- ProjectCS/Assets/WorkSpace/Nakamachi/Script/PlayerLandingSound.cs
+++ ProjectCS/Assets/WorkSpace/Nakamachi/Script/PlayerLandingSound.cs
@@ -3,28 +3,56 @@
 //最終更新日:2025/04/15
 //[Log]
 //04/15　中町　Playerが着地したときの効果音処理
+//04/16　中町　着地したら一回だけ鳴らす処理に修正
 
 using UnityEngine;
 
 public class PlayerLandingSound : MonoBehaviour
 {
-    public AudioSource LandingSound;
-    private bool IsGrounded;
-    private bool WasGrounded;
+    //着地音のAudioClipを設定するための変数
+    public AudioClip LandingSound;
 
-    // Update is called once per frame
-    void Update()
+    //AudioSourceコンポーネントを保持するための変数
+    private AudioSource AudioSource;
+
+    //プレイヤーが着地したかどうかを判定するための変数
+    private bool HasLanded = false;
+
+    void Start()
     {
-        //地面にいるかどうかをチェック
-        IsGrounded = Physics.Raycast(transform.position, Vector3.down, 1.1f);
+        //AudioSourceコンポーネントを取得
+        AudioSource = GetComponent<AudioSource>();
+    }
 
-        //着地したとき
-        if(!WasGrounded&&IsGrounded)
+    //プレイヤーが地面に着地したときに呼ばれるメソッド
+    void OnCollisionEnter(Collision collision)
+    {
+        //衝突したオブジェクトがGroundタグを持っていて、まだ着地していないとき
+        if (collision.gameObject.CompareTag("Ground") && !HasLanded)
         {
-            LandingSound.Play();
-        }
+            //プレイヤーが着地したとき
+            HasLanded = true;
 
-        //前のフレームの状態を更新
-        WasGrounded = IsGrounded;
+            //着地音を再生
+            PlayLandingSound();
+        }
+    }
+
+    //プレイヤーが地面から離れたときに呼ばれるメソッド
+    void OnCollisionExit(Collision collision)
+    {
+        //衝突したオブジェクトがGroundタグを持っているとき
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            //プレイヤーが地面から離れたとき
+            HasLanded = false;
+        }
+    }
+
+    //着地音を再生するメソッド
+    void PlayLandingSound()
+    {
+        //AudioSourceコンポーネントを使用して着地音を再生
+        AudioSource.PlayOneShot(LandingSound);
     }
 }
