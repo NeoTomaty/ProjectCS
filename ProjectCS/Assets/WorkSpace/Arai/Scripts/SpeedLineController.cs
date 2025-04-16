4eb7315b426c44e401ea73e8f84d0bdf4f16baf9
AraiShu <arai.2001.shu@gmail.com>
2025-04-16 18:14:27 +0900

    集中線がUVアニメーションで動くように変更

--- ProjectCS/Assets/WorkSpace/Arai/Scripts/SpeedLineController.cs
+++ ProjectCS/Assets/WorkSpace/Arai/Scripts/SpeedLineController.cs
@@ -1,18 +1,27 @@
 //======================================================
 // [SpeedLineController]
 // 作成者：荒井修
-// 最終更新日：04/13
+// 最終更新日：04/16
 // 
 // [Log]
 // 04/13　荒井　プレイヤーの速度に応じて集中線のスケールが変化するように実装
+// 04/16　荒井　テクスチャのUVアニメーションを実装
 //======================================================
 
 using UnityEngine;
+using UnityEngine.UI;
 
 // 集中線オブジェクトにアタッチ
 public class SpeedLineController : MonoBehaviour
 {
     [SerializeField] private PlayerSpeedManager PlayerSpeedManager;
+
+    // UVアニメーション設定
+    [SerializeField] private RawImage SpeedLineImage;           // 集中線のRawImageコンポーネント
+    [SerializeField] private int TotalFrame = 6;                // 総フレーム数
+    [SerializeField] private float AnimationDuration = 0.1f;    // アニメーション進行ペース
+    private int CurrentFrame = 0; // 現在のフレーム数
+    private float Timer = 0.0f;
 
     // スケールの設定
     [SerializeField] private float LowSpeedScale = 1.5f;      // 低速時
@@ -27,6 +36,11 @@ public class SpeedLineController : MonoBehaviour
     void Start()
     {
         transform.localScale = new Vector3(LowSpeedScale, LowSpeedScale, 1.0f); // 初期値を設定
+
+        if (SpeedLineImage == null) return;
+        Rect rect = SpeedLineImage.uvRect;
+        rect.height = 1.0f / TotalFrame;
+        SpeedLineImage.uvRect = rect;
     }
 
     // Update is called once per frame
@@ -45,5 +59,22 @@ public class SpeedLineController : MonoBehaviour
 
         // スケールを設定
         transform.localScale = new Vector3(Scale, Scale, 1.0f);
+
+
+        // UVアニメーション
+        if (SpeedLineImage == null) return;
+        Timer += Time.deltaTime;
+        // 一定時間ごとにアニメーション進行
+        if (Timer >= AnimationDuration)
+        {
+            Timer = 0.0f;   // タイマーリセット
+            CurrentFrame++; //アニメーションフレーム加算
+            CurrentFrame %= TotalFrame;
+
+            // 表示箇所切り換え
+            Rect rect = SpeedLineImage.uvRect;
+            rect.y = rect.height * CurrentFrame;
+            SpeedLineImage.uvRect = rect;
+        }
     }
 }
