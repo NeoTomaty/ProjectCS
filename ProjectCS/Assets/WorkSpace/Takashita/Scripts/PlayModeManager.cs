3f42d621ffaef0454848bfec6602f86cc0419947
dakkie <yon4daiki@gmail.com>
2025-04-22 00:12:59 +0900

    高下 PlayerModeManager.cs一部追加処理

--- ProjectCS/Assets/WorkSpace/Takashita/Scripts/PlayModeManager.cs
+++ ProjectCS/Assets/WorkSpace/Takashita/Scripts/PlayModeManager.cs
@@ -6,7 +6,7 @@
 // 
 // [Log]
 // 04/21 高下 スクリプト作成 
-// 
+// 04/21 高下 CPUのインプット操作を切り替える処理を追加
 // 
 //====================================================
 using UnityEngine;
@@ -17,6 +17,7 @@ using UnityEngine;
 // 3. PlayerCamera1にはプレイヤー1のカメラをセット
 // 4. PlayerCamera2にはプレイヤー2(またはCPU1)のカメラをセット
 // 5. ModeをSolo(1人プレイ)またはTwoPlayer(2人プレイ)に設定
+// 6. PlayerObject2にPlayer2をセットする
 
 public class PlayModeManager : MonoBehaviour
 {
@@ -26,6 +27,9 @@ public class PlayModeManager : MonoBehaviour
         TwoPlayer,  // 2人プレイ
     }
 
+    [Tooltip("プレイヤー2のオブジェクトをアタッチ")]
+    [SerializeField]
+    private GameObject PlayerObject2;
     [Tooltip("プレイヤー1のカメラオブジェクトをアタッチ")]
     [SerializeField]
     private GameObject PlayerCamera1;
@@ -36,9 +40,12 @@ public class PlayModeManager : MonoBehaviour
     [SerializeField]
     private PlayMode Mode;
 
-
     void Start()
     {
+        if (!PlayerObject2)
+        {
+            Debug.LogError("プレイヤー2がアタッチされてません");
+        }
         if (!PlayerCamera1)
         {
             Debug.LogError("プレイヤーカメラ1がアタッチされてません");
@@ -47,24 +54,42 @@ public class PlayModeManager : MonoBehaviour
         {
             Debug.LogError("プレイヤーカメラ2がアタッチされてません");
         }
-
+       
         // PlayerCamera1のCameraComponent
         Camera Camera1 = PlayerCamera1.gameObject.GetComponent<Camera>();
 
         // PlayerCamera2のCameraComponent
         Camera Camera2 = PlayerCamera2.gameObject.GetComponent<Camera>();
 
+        // Player2の各操作系Component
+        LRMovePlayer LRMovePlayer2 = PlayerObject2.GetComponent<LRMovePlayer>();
+        JumpPlayer JumpPlayer2 = PlayerObject2.GetComponent<JumpPlayer>();
+        PlayerDeceleration DecelerationPlayer2 = PlayerObject2.GetComponent<PlayerDeceleration>();
+
         switch (Mode)
         {
             // ソロ専用モード(ビューポートを画面全体に拡張)
             case PlayMode.Solo:
                 this.PlayerCamera2.SetActive(false); // PlayerCamera2は非アクティブにしておく
+
+                // ソロプレイの場合プレイヤー2はCPU扱いなので
+                // 操作関連のアクティブ状態をfalseにしておく
+                LRMovePlayer2.enabled = false;
+                JumpPlayer2.enabled = false;
+                DecelerationPlayer2.enabled = false;
+
                 Camera1.rect = new Rect(0f, 0f, 1f, 1f); // 画面全体
                 break;
 
             // 2人プレイ専用モード(ビューポートを左右に分割)
             case PlayMode.TwoPlayer:
                 this.PlayerCamera2.SetActive(true); // PlayerCamera2をアクティブにする
+
+                // プレイヤー2の操作関連のアクティブ状態をtrueにしておく
+                LRMovePlayer2.enabled = true;
+                JumpPlayer2.enabled = true;
+                DecelerationPlayer2.enabled = true;
+
                 Camera1.rect = new Rect(0f, 0f, 0.5f, 1f);   // 左半分
                 Camera2.rect = new Rect(0.5f, 0f, 0.5f, 1f); // 右半分
                 break;
