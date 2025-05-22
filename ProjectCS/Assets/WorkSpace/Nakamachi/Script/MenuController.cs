0eac2dcdb9bca3e6dcfe82e0fb88eb5df5ca612d
Raiga1119 <marvel_0819@icloud.com>
2025-05-22 16:59:40 +0900

    中町　タイトル画面のキーボード操作とコントローラー操作のバグ修正

--- ProjectCS/Assets/WorkSpace/Nakamachi/Script/MenuController.cs
+++ ProjectCS/Assets/WorkSpace/Nakamachi/Script/MenuController.cs
@@ -1,48 +1,146 @@
+//MenuController.cs
+//作成者:中町雷我
+//最終更新日:2025/05/22
+//アタッチ:StartButtonにアタッチ
+//[Log]
+//05/22　中町　タイトルのキーボード操作とコントローラー操作処理
+
 using UnityEngine;
 using UnityEngine.UI;
 
 public class MenuController : MonoBehaviour
 {
+    //スタートボタン、終了ボタン、オプションボタンをインスペクターから設定
     public Button StartButton;
     public Button ExitButton;
     public Button OptionButton;
+
+    //ボタンを配列で管理
     private Button[] Buttons;
+
+    //現在選択されているボタンのインデックス
     private int SelectedIndex = 0;
+
+    //入力の間隔(連続入力を防ぐためのディレイ)
+    public float InputDelay = 0.3f;
+    private float InputTimer = 0.0f;
 
     void Start()
     {
-        Buttons = new Button[] { StartButton, ExitButton, OptionButton };
+        //ボタンを配列に格納
+        Buttons = new Button[] 
+        {
+            StartButton, ExitButton, OptionButton
+        };
+
+        //初期選択状態を更新
         UpdateButtonSelection();
     }
 
     void Update()
     {
+        //入力タイマーを減少させる
+        InputTimer -= Time.deltaTime;
+
+        //水平方向の入力を取得(キーボードの左右キーやゲームパッドのスティック)
         float HorizontalInput = Input.GetAxis("Horizontal");
 
-        if (HorizontalInput > 0.5f)
+        //入力ディレイが経過している場合のみ処理
+        if (InputTimer <= 0f)
         {
-            SelectedIndex = (SelectedIndex + 1) % Buttons.Length;
-            UpdateButtonSelection();
-        }
-        else if (HorizontalInput < -0.5f)
-        {
-            SelectedIndex = (SelectedIndex - 1 + Buttons.Length) % Buttons.Length;
-            UpdateButtonSelection();
+            int PreviousIndex = SelectedIndex;
+            int NextIndex = SelectedIndex;
+
+            //右入力:次のボタンへ移動
+            if (HorizontalInput > 0.5f)
+            {
+                if (SelectedIndex == 0)
+                {
+                    NextIndex = 1;
+                }
+                else if (SelectedIndex == 1)
+                {
+                    NextIndex = 2;
+                }
+            }
+            //左入力:前のボタンへ移動
+            else if (HorizontalInput < -0.5f)
+            {
+                if (SelectedIndex == 2)
+                {
+                    NextIndex = 1;
+                }
+                else if (SelectedIndex == 1)
+                {
+                    NextIndex = 0;
+                }
+            }
+
+            //選択が変わったとき、見た目を更新
+            if (NextIndex != SelectedIndex)
+            {
+                //前のボタンのスケールをリセット
+                ResetButtonScale(SelectedIndex);
+
+                //新しい選択に更新
+                SelectedIndex = NextIndex;
+
+                //入力ディレイをリセット
+                InputTimer = InputDelay;
+
+                //ボタンの見た目を更新
+                UpdateButtonSelection();
+            }
         }
 
+        //決定ボタンが押されたら、現在選択中のボタンのクリックイベントを実行
         if (Input.GetButtonDown("Submit"))
         {
             Buttons[SelectedIndex].onClick.Invoke();
         }
     }
 
+    //ボタンのスケールを元に戻す(選択解除時)
+    void ResetButtonScale(int index)
+    {
+        Text ButtonText = Buttons[index].GetComponentInChildren<Text>();
+
+        if (ButtonText != null)
+        {
+            ButtonText.transform.localScale = Vector3.one;
+        }
+    }
+
+    //ボタンの選択状態に応じて色やスケールを変更
     void UpdateButtonSelection()
     {
-        foreach (Button button in Buttons)
+        for (int i = 0; i < Buttons.Length; i++)
         {
-            button.GetComponent<Image>().color = Color.white;
-        }
+            Image ButtonImage = Buttons[i].GetComponent<Image>();
+            Text ButtonText = Buttons[i].GetComponentInChildren<Text>();
 
-        Buttons[SelectedIndex].GetComponent<Image>().color = Color.yellow;
+            if (i == SelectedIndex)
+            {
+                //選択中のボタン:文字を赤く、大きくする
+                ButtonImage.color = Color.white;
+
+                if (ButtonText != null)
+                {
+                    ButtonText.color = Color.red;
+                    ButtonText.transform.localScale = Vector3.one * 1.3f;
+                }
+            }
+            else
+            {
+                //非選択のボタン:文字を黒く、通常サイズに戻す
+                ButtonImage.color = Color.white;
+
+                if (ButtonText != null)
+                {
+                    ButtonText.color = Color.black;
+                    ButtonText.transform.localScale = Vector3.one;
+                }
+            }
+        }
     }
 }
