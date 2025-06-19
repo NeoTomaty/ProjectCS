d03d0978424cd700ef53518258b0943f2ab46986
Raiga1119 <marvel_0819@icloud.com>
2025-06-19 20:26:45 +0900

    中町　ステージ3のトランポリンSE実装

--- ProjectCS/Assets/WorkSpace/Arai/Scripts/Trampolin.cs
+++ ProjectCS/Assets/WorkSpace/Arai/Scripts/Trampolin.cs
@@ -1,16 +1,30 @@
 //======================================================
 // [Trampolin]
 // 作成者：荒井修
-// 最終更新日：05/20
+// 最終更新日：06/19
 // 
 // [Log]
 // 05/20　荒井　触れたプレイヤーをジャンプさせる処理を実装
+// 06/19　中町　トランポリンSE実装
 //======================================================
 using UnityEngine;
 
 public class Trampolin : MonoBehaviour
 {
     [SerializeField] private float JumpPower = 100f; // ジャンプ力
+
+    [SerializeField] private AudioClip JumpSE;
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+
+        if(audioSource == null)
+        {
+            audioSource = gameObject.AddComponent<AudioSource>();
+        }
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
@@ -23,6 +37,11 @@ public class Trampolin : MonoBehaviour
             {
                 // ジャンプ力を加える
                 playerRigidbody.AddForce(Vector3.up * JumpPower, ForceMode.Impulse);
+
+                if(JumpSE != null)
+                {
+                    audioSource.PlayOneShot(JumpSE);
+                }
             }
         }
     }
