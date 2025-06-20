8295b4777f4c2206067fe71cc3b8f38e4286becc
Raiga1119 <marvel_0819@icloud.com>
2025-06-20 16:17:46 +0900

    中町　作業途中

--- ProjectCS/Assets/WorkSpace/Nakamachi/Script/ImageButtonEffects.cs
+++ ProjectCS/Assets/WorkSpace/Nakamachi/Script/ImageButtonEffects.cs
@@ -1,17 +1,23 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 
+//このクラスは、UIボタンに対してマウスオーバーや選択時に拡大する視覚効果を与えるためのもの
+//IPointerEnterHandler,IPointerExitHandler,ISelectHandler,IDeselectHandlerを実装することで、マウスやキーボードによるインタラクションに対応する
 public class ImageButtonEffects : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, ISelectHandler, IDeselectHandler
 {
+    //元のスケール(サイズ)を保持するための変数
     private Vector3 OriginalScale;
 
+    //ゲームオブジェクトの元のスケールを保存する
     void Start()
     {
         OriginalScale = transform.localScale;
     }
 
+    //マウスカーソルがボタン上に乗ったときに呼ばれる処理
     public void OnPointerEnter(PointerEventData EventData)
     {
+        //ボタンのスケールを1.2倍に拡大
         transform.localScale = OriginalScale * 1.2f;
     }
 
